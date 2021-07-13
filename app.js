a47e33c66f5f6855027f66bd309d8b060ba8cf3c
AjeethKumar R <rajeethkumarind01@gmail.com>
2021-07-13 15:12:46 +0530

    app complete css pending

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-var btnTranslate = document.querySelector("btn-translate")
-var txtInput = document.querySelector("#txt-input")
+var btn = document.querySelector("#btnTranslate")
+var engText = document.querySelector("#inputRead")
 var outputDiv = document.querySelector("#output")
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+btn.addEventListener("click",clickFn)
+var serverUrl= "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL +"?"+"text"+text
+function getUrl(text){
+    return serverUrl +"?" + "text=" + text
 }
 
-function clickHandler(){
-    var inputText = txtInput.value
-    fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translatedText
-        outputDiv.innertext = translatedText
-    })
-
-
+function errorHandler(error){
+    console.log("error-occured",error)
+    alert("check you internet connection")
 }
 
-btnTranslate.addEventListener("click",clickHandler)
+function clickFn(){
+   var inputText = engText.value
+   fetch(getUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var outputText = json.contents.translated
+            outputDiv.innerText = outputText
+   })
+   .catch(errorHandler)
+}
